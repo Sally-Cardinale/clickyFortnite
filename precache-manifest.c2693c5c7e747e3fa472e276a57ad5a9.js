ce5083b1a7a488a195c0bc88030315199c123c2f
Sally Rae <sally.cardinale@gmail.com>
2019-06-12 12:51:10 -0700

    Updates

---
+++ precache-manifest.c2693c5c7e747e3fa472e276a57ad5a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbc313087b5ba6aa50f9f70e511b34b",
+    "url": "/clickyFortnite/index.html"
+  },
+  {
+    "revision": "c169412c06e9c19bd558",
+    "url": "/clickyFortnite/static/css/main.804a2757.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickyFortnite/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "c169412c06e9c19bd558",
+    "url": "/clickyFortnite/static/js/main.a269f5d0.chunk.js"
+  },
+  {
+    "revision": "c07de5ce5acbc4a45cc4",
+    "url": "/clickyFortnite/static/js/runtime~main.d149c71e.js"
+  }
+]);
